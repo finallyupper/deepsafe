fdf55a290d4f3e198eba0ac82fd0e473133c6865
finallyupper <dianaoh1021@gmail.com>
2025-01-30 20:51:14 +0900

    Modified: fixed demo

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ warnings.filterwarnings('ignore')
 from PIL import Image
 from torchvision.transforms import transforms
 import numpy as np 
+MESSAGE_SIZE = 4
 
 _DDF_MODEL_MAPPINGS = {
     "byeon_cha": "/home/yoojinoh/Others/ckpt_best_img_lam1_al2.pt",
@@ -14,25 +15,32 @@ _DDF_MODEL_MAPPINGS = {
 }
 
 _TEST_CONFIGS = {
-    "message_size": 4,
+    "message_size": MESSAGE_SIZE,
     "height": 160,
     "width": 160
 }
 
-def assign_messages(users, message_size=4, device='cpu'):
+#NOTE(Yoojin): 임시 유저-메세지 데이터
+USER_WATERMARK_IDS = {
+    "byeon": [0., 1., 0., 1.],
+    "cha": [1., 1., 0., 0.]
+} 
+
+def assign_messages(users, device='cpu'):
     num_users = len(users)
     max_users = 16
     assert num_users <= max_users, f"The number of maximum users is {max_users}."
-    unique_messages = torch.eye(max_users, message_size, dtype=torch.float).to(device)
+    unique_messages = torch.eye(max_users, MESSAGE_SIZE, dtype=torch.float).to(device)
     user_messages = {user: unique_messages[i].tolist() for i, user in enumerate(users)}
     #TODO(Yoojin): Save info to DB
     return user_messages    
 
-def pred_user(image):
-    pred_message_id = decode_image(image)
-    #TODO(Yoojin): 입력된 메시지와 일치하는 사용자 검색
-    return None 
-
+def find_message2user(message:list)->str:
+    for key, value in USER_WATERMARK_IDS.items():
+        if value == message:
+            return key
+        return None  # ex. 'byeon'
+    
 def encode_image(model, image, message):
     """insert watermark into original image"""
     if len(image.shape) == 3:
@@ -67,7 +75,7 @@ def load_model(model_type, mode = 'eval', device=None):
         model.decoder.eval()
     return model 
 
-def apply_faceswap(model_type, swapped_image_path, src_path, tgt_path):
+def apply_faceswap(model_type, swapped_image_path, src_path, tgt_path, encoded=True):
     """
     Apply dual defense and save the faceswapped image (swap face a with face b)
 
@@ -76,6 +84,7 @@ def apply_faceswap(model_type, swapped_image_path, src_path, tgt_path):
         swapped_image_path (str) : path to save the face swapped image
         src_path (str): path of (encoded) image a
         tgt_path (str): path of (encoded) image b
+        encoded (bool): if the given image is encoded or not
     """
     device = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')
     assert model_type in _DDF_MODEL_MAPPINGS.keys() 
@@ -100,14 +109,27 @@ def apply_faceswap(model_type, swapped_image_path, src_path, tgt_path):
 
         image_a_deepfake =(_image_a_deepfake[0]  * 255).permute(1, 2, 0).detach().cpu().numpy()
         image_b_deepfake =(_image_b_deepfake[0]  * 255).permute(1, 2, 0).detach().cpu().numpy()
+
         save_image(image_a_deepfake, os.path.join(swapped_image_path, 'output_A2B.png'))
         save_image(image_b_deepfake, os.path.join(swapped_image_path, 'output_B2A.png'))
+
+        if encoded:
+            _pred_a_message = model.decode(_image_a_deepfake)        
+            _pred_b_message = model.decode(_image_b_deepfake) 
+
+            pred_a_message = list(map(int, _pred_a_message[0])) 
+            pred_b_message = list(map(int, _pred_b_message[0])) 
+        
+            usera = find_message2user(pred_a_message)
+            userb = find_message2user(pred_b_message)
+
+            print(f'>> Someone tried to make deepfake with user {usera} and user {userb}')
 
         # restore_original(original_src_image, _image_a_deepfake[0], src_coord, os.path.join(swapped_image_path, 'output_A2B.png')) 
         # restore_original(original_tgt_image, _image_b_deepfake[0], tgt_coord, os.path.join(swapped_image_path, 'output_B2A.png'))  
 
 def crop_image(image_path):
-    cascade = cv2.CascadeClassifier('/home/yoojinoh/Others/deepsafe/data/haarcascade_frontalface_alt.xml')
+    cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt.xml')
     image = cv2.imread(image_path)
     results = cascade.detectMultiScale(image)
 
@@ -118,16 +140,21 @@ def crop_image(image_path):
     (x, y, w, h) = crop_coord
     cropped_face = image[y:y+h, x:x+w]
     original_image = image.copy()
-
+    print("Sucessfully Cropped image")
     return original_image, cropped_face, crop_coord
 
 def restore_original(original_image, encoded_face, coord, result_path):
     (x, y, w, h) = coord 
+
     encoded_face = (encoded_face * 255).permute(1, 2, 0).detach().cpu().numpy()
+    encoded_face = np.clip(encoded_face, 0, 255)
+
     resized_encoded_face = cv2.resize(encoded_face, (w, h))
+    
     result_image = original_image.astype(np.float32)
     result_image[y:y+h, x:x+w] = resized_encoded_face
-    save_image(result_image, result_path)
+    save_image(result_image, result_path) 
+    print("Restored to original image")
     
 def crop_and_encode_image(model_type, image_path, message, device, alpha=1.0):
     """
@@ -135,7 +162,7 @@ def crop_and_encode_image(model_type, image_path, message, device, alpha=1.0):
     args:
         model_type (str): ['byeon_cha', 'win_chuu'] 
         image_path (str): path of the image to encode
-        message (torch.Tensor) : ex. torch.randint(0, 2, (1, 4), dtype=torch.float).to(device).detach()
+        message (list) : [1., 0., 1] ->ex. torch.randint(0, 2, (1, 4), dtype=torch.float).to(device).detach()
         device : cpu or gpu
         alpha (float) : the amount to blend encoded image with original image 
     """
@@ -143,8 +170,9 @@ def crop_and_encode_image(model_type, image_path, message, device, alpha=1.0):
     transformed_cropped_face = get_transform(cropped_face, 160, 160).to(device)
 
     model = load_model(model_type, 'eval', device)
-
+    message = torch.FloatTensor(message).to(device).detach()
     encoded_face = encode_image(model, transformed_cropped_face, message)[0]
+    print('Successfully encoded image')
     result_path = os.path.join(os.path.dirname(image_path), 'encoded_' + os.path.basename(image_path))
     restore_original(original_image, encoded_face, (x, y, w, h), result_path) 
 
@@ -152,37 +180,15 @@ def crop_and_encode_image(model_type, image_path, message, device, alpha=1.0):
 
 if __name__ == "__main__":
     device = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')
-    random_message = torch.randint(0, 2, (1, 4), dtype=torch.float).to(device).detach()
-    crop_and_encode_image('byeon_cha', '/home/yoojinoh/Others/byeon_total3.jpg', random_message, device) 
-
+    #TODO(Yoojin): 개별 user를 위한 watermark information으로 수정
+    crop_and_encode_image('byeon_cha', 
+                          '/home/yoojinoh/Others/byeon_total5.jpg', 
+                          USER_WATERMARK_IDS['byeon'], 
+                          device) 
+    crop_and_encode_image('byeon_cha', 
+                          '/home/yoojinoh/Others/cha_total2.jpg', 
+                          USER_WATERMARK_IDS['cha'], 
+                          device) 
     apply_faceswap(model_type="byeon_cha",  swapped_image_path="/home/yoojinoh/Others/", 
-                   src_path="/home/yoojinoh/Others/byeon_total3.jpg", 
-                   tgt_path="/home/yoojinoh/Others/cha_total.jpg")
-
-
-"""
-def crop_and_encode_image(model_type, image_path, message, device):
-    cascade = cv2.CascadeClassifier('/home/yoojinoh/Others/deepsafe/data/haarcascade_frontalface_alt.xml')
-    image = cv2.imread(image_path)
-    results = cascade.detectMultiScale(image)     
-    crop_coord = results[0] 
-    x, y, w, h = crop_coord
-    cropped_face = image[y:y+h, x:x+w]
-    original_image = image.copy()
-    #crop_coord, cropped_face, original_image = crop_image(image_path) # bgr image
-
-    transformed_cropped_face = get_transform(cropped_face, 160, 160).to(device) # resize image
-    model = load_model(model_type, 'eval', device)
-    encoded_face = encode_image(model, transformed_cropped_face, message)[0] 
-
-    x, y, w, h = crop_coord
-    encoded_face = (encoded_face * 255).permute(1, 2, 0).detach().cpu().numpy()
-    resized_encoded_face = cv2.resize(encoded_face, (w, h))
-
-    result_image = original_image.copy().astype(np.float32)
-    result_image[y:y+h, x:x+w] = resized_encoded_face
-
-    result_path = os.path.join(os.path.dirname(image_path), 'encoded_' + os.path.basename(image_path))
-    save_image(result_image, result_path)
-
-"""
+                   src_path="/home/yoojinoh/Others/encoded_byeon_total5.jpg", 
+                   tgt_path="/home/yoojinoh/Others/encoded_cha_total2.jpg")
